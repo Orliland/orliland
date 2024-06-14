515701de9bf10be87f7b14b29ed3e9bf47ce66a2
Orlando Verdin <git@orli.land>
2024-06-13 19:37:07 -0700

    add base footer structure

---
+++ app/ui/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="container mx-auto leading-4 text-center text-black/40">
+      <p className="text-base font-medium mt-[60px]">
+        @Orliland - Orlando Verdin
+      </p>
+      <p className="text-xs mt-[120px] mb-[60px]">
+        Designed by a{" "}
+        <a className="font-bold" href="https://vjy.me" target="__blank">
+          wizard
+        </a>
+        . Code by me.
+      </p>
+    </footer>
+  );
+}
