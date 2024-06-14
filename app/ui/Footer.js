59187ef4fe6dc31388bfee974143757b5401c8de
Orlando Verdin <git@orli.land>
2024-06-13 19:52:36 -0700

    add styles and background to footer

--- app/ui/Footer.js
+++ app/ui/Footer.js
@@ -1,10 +1,14 @@
+import styles from "@/app/ui/footer.module.css";
+
 export default function Footer() {
   return (
-    <footer className="container mx-auto leading-4 text-center text-black/40">
-      <p className="text-base font-medium mt-[60px]">
+    <footer
+      className={`${styles.footer} container mx-auto leading-4 text-center py-[60px]`}
+    >
+      <p className="text-base font-medium text-gray-600/60">
         @Orliland - Orlando Verdin
       </p>
-      <p className="text-xs mt-[120px] mb-[60px]">
+      <p className="text-xs mt-[120px] text-black/20">
         Designed by a{" "}
         <a className="font-bold" href="https://vjy.me" target="__blank">
           wizard
