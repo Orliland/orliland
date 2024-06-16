9f0067ac22c5df92a10553a35529770a6f5c59c5
Orlando Verdin <git@orli.land>
2024-06-15 19:18:35 -0700

    add header and basic button border

---
+++ app/ui/Buttons.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+import DownloadIcon from "@/public/download.svg";
+
+export function ButtonBorder() {
+  return (
+    <div className="sunset2-border ">
+      <div className="bg-white rounded-3xl ">
+        <a
+          href="#"
+          className="flex items-center gap-2 text-base font-medium leading-4 sunset2 rounded-3xl px-6 py-3 "
+        >
+          <span>CV</span>
+
+          <Image
+            alt="download cv icon"
+            src={DownloadIcon}
+            className="w-6 h-6"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
