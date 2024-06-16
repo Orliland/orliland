d0930193d6f0f8944de930ded7270521600edb16
Orlando Verdin <git@orli.land>
2024-06-15 20:52:12 -0700

    update button

--- app/ui/Footer.js
+++ app/ui/Footer.js
@@ -1,20 +1,22 @@
 import styles from "@/app/ui/footer.module.css";
 
 export default function Footer() {
+  // TODO: Remove mt-[430 despues de agregar contenido]
   return (
-    <footer
-      className={`${styles.footer} container mx-auto leading-4 text-center py-[60px]`}
-    >
-      <p className="text-base font-medium text-gray-600/60">
-        @Orliland - Orlando Verdin
-      </p>
-      <p className="text-xs mt-[120px] text-black/20">
-        Designed by a{" "}
-        <a className="retro2" href="https://vjy.me" target="__blank">
-          wizard
-        </a>
-        . Code by me.
-      </p>
-    </footer>
+    <>
+      <div className={styles.footer}></div>
+      <footer className="container mx-auto mt-[430px] leading-4 text-center py-[60px]">
+        <p className="text-base font-medium text-gray-600/60">
+          @Orliland - Orlando Verdin
+        </p>
+        <p className="text-xs mt-[120px] text-black/20">
+          Designed by a{" "}
+          <a className="retro2" href="https://vjy.me" target="__blank">
+            wizard
+          </a>
+          . Code by me.
+        </p>
+      </footer>
+    </>
   );
 }
